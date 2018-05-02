e7c018897efb0727b14ff97fc5001278f723c0a1
khoaprogrammer <khoa.developer@gcloud.pro>
2018-05-02 09:36:45 +0700

    update

--- CIF/CIF.DB/ArchModel.cs
+++ CIF/CIF.DB/ArchModel.cs
@@ -5,10 +5,17 @@ using System.Web;
 
 namespace CIF.DB
 {
+    public enum ModelType
+    {
+        MAX3D,
+        SKETCHUP
+    }
+
     public class ArchModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string DownloadLink { get; set; }
+        public ModelType Type { get; set; }
     }
 }
